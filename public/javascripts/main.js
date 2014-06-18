75899629dfdc8ebdb68d8ae71da67975f58742d9
Alex Emergy <alex.emergy@gmail.com>
2014-06-18 12:37:18 +0400

    eroiufh

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,45 +3,7 @@ $(document).ready(function() {
 
     $('.menu-item').each(function( index ) {
         if ($(this).attr('href') == current_url) {
-            //$(this).addClass('active');
             $(this).attr("class", "active");
-        }
-    });
-
-    $('#buttonLogin').click(function() {
-        $('#buttonLogin').attr("disabled", "disabled");
-
-        var login = $('#inputLogin').val();
-        var password = hex_md5($('#inputPassword').val());
-
-        $.ajax({
-            type: "POST",
-            url: "/login",
-            data: "login=" + login + "&password=" + password,
-            success: function(msg) {
-                $('#buttonLogin').removeAttr("disabled");
-                console.log(msg);
-
-                if (msg == '0') {
-                    $('#login-error').show();
-                } else {
-                    $('#login-error').hide();
-                    window.location.replace('/');
-                }
-            },
-            error: function (XMLHttpRequest, textStatus, errorThrown) {
-                console.log("XMLHttpRequest: " + XMLHttpRequest);
-                console.log("textStatus: " + textStatus);
-                console.log("errorThrown: " + errorThrown);
-            }
-        });
-    });
-
-
-
-    $(".form-control").keypress(function(e) {
-        if (e.which == 13) {
-            $('#buttonLogin').click();
         }
     });
 });
